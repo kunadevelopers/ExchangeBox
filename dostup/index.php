22fdb39af6cca229c05cd5d5f81553acf7348255
maxd <maks.drik@gmail.com>
2019-05-29 11:49:53 +0300

    add zip

--- dostup/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Внимание! Не удаляйте кавычки при указание ваших значений.
-Urgent! Do not delete quotation marks while entering data
-*/
-$marr=array();
-$marr['KEY'] = ""; 		// API ключ / API Key
-$marr['SECRET'] = ""; 	// Секрет ключ / Secret Key
